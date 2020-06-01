7c8d0d63d77271c11987090e647d13ed36149107
sanyokstd <59456430+sanyokstd@users.noreply.github.com>
2020-06-01 11:58:44 +0300

    1
    1

--- build/js/main.js
+++ build/js/main.js
@@ -14,6 +14,16 @@ $(function(){
              $('.nav__level2').removeClass('open')
              $(this).find('.nav__level2').addClass('open')
          })
+
+         $('.mail__left__level1').mouseenter(function(){
+            $('.nav__level2').removeClass('open')
+            $(this).find('.nav__level2').addClass('open')
+            $('.overlay').addClass('active')
+        })
+        $('.mail__left__level1').mouseleave(function(){
+            $('.nav__level2').removeClass('open')
+            $('.overlay').removeClass('active')
+        })
          $('.nav__level2').mouseleave(function() {
              $('.nav__level2').removeClass('open')
          })
@@ -28,6 +38,11 @@ $(function(){
     }
     if($(window).width() <= 767){
        $('.menu-catalog__burger').on('touchend', function(){
+        $('.header__mob').toggleClass('active')
+        $('.mob__menu').slideToggle(200)
+        $('.overlay').toggleClass('active')
+       })
+       $('.header__mob__phone').on('touchend', function(){
         $('.header__mob').toggleClass('active')
         $('.mob__menu').slideToggle(200)
         $('.overlay').toggleClass('active')
